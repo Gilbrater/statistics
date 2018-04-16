49c6979389291f970d534e7d62206241a92cbc49
gilbrater <gilbrater1@gmail.com>
2018-04-16 04:18:28 +0100

    Handle statistics call

--- src/main/java/com/statistics/core/utils/TransactionStoreUtil.java
+++ src/main/java/com/statistics/core/utils/TransactionStoreUtil.java
@@ -55,6 +55,37 @@ public class TransactionStoreUtil{
     }
 
     public Statistics getStatistics(long currentTime, long interval){
-        return new Statistics();
+        Statistics statistics = new Statistics();
+        double totalAmount = 0;
+        long totalNumberOfTransactions = 0;
+        double max = Double.MIN_VALUE;
+        double min = Double.MAX_VALUE;
+        double average = 0;
+
+        double secondsMax;
+        double secondsMin;
+
+        for(long i=currentTime; i>currentTime-interval; i--){
+            SecondsSummary secondsSummary = transactionSummaryPerSecond.get(i);
+            if(secondsSummary!=null){
+                totalAmount+=secondsSummary.getTotalAmount();
+                totalNumberOfTransactions+=secondsSummary.getNumberOfTransactions();
+                max = Math.max(max, secondsSummary.getMax());
+                min = Math.min(min, secondsSummary.getMin());
+            }
+        }
+        if(totalNumberOfTransactions==0){
+            max=0;
+            min=0;
+        }else{
+            average = totalAmount/totalNumberOfTransactions;
+        }
+
+        statistics.setSum(totalAmount);
+        statistics.setAvg(average);
+        statistics.setCount(totalNumberOfTransactions);
+        statistics.setMax(max);
+        statistics.setMin(min);
+        return statistics;
     }
 }
